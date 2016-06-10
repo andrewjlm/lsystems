2403b5bb65625b9f230320664a2b88ff0580d249
Andrew Mullins <andrew.jamesmullins@gmail.com>
2016-06-09 22:39:29 -0700

    Koch curve in Bokeh

--- lsystem.py
+++ lsystem.py
@@ -1,6 +1,43 @@
+from bokeh.plotting import figure, output_file, show
 import json
-import turtle
+from math import sin, cos, radians
 from sys import argv
+
+class Point(object):
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+    def __add__(self, other):
+        newx = self.x + other.x
+        newy = self.y + other.y
+        return Point(newx, newy)
+
+    def __repr__(self):
+        return "({0}, {1})".format(self.x, self.y)
+
+class State(object):
+    def __init__(self, angle):
+        self.current = Point(0, 0)
+        self.steps = [self.current]
+        self.movement = Point(10, 0)
+        self.angle = angle
+
+
+    def rotate_movement(self, angle):
+        State.rotate_point(self.movement, angle)
+
+    def rotate_point(point, angle):
+        # Rotate point around origin
+        s = sin(radians(angle))
+        c = cos(radians(angle))
+
+        xnew = point.x * c - point.y * s
+        ynew = point.y * c + point.x * s
+
+        point.x = xnew
+        point.y = ynew
+
 
 def apply_rules(rules, state):
     result = list()
@@ -14,16 +51,19 @@ def apply_rules(rules, state):
     # Join the list into one string
     return "".join(result)
 
-def draw_state(step, pen):
-    print(step)
-    for c in step:
-        if c == 'F':
-            pen.forward(10)
-        elif c == '+':
-            # TODO: Need to read the angle from somewhere
-            pen.left(90)
-        elif c == '-':
-            pen.right(90)
+def parse_step(step, state):
+    if step == 'F':
+        def draw_step(state):
+            state.current += state.movement
+            state.steps.append(state.current)
+    elif step == '+':
+        def draw_step(state):
+            state.rotate_movement(state.angle)
+    elif step == '-':
+        def draw_step(state):
+            state.rotate_movement(-state.angle)
+
+    return draw_step
 
 if __name__ == '__main__':
     system_file = argv[1]
@@ -34,19 +74,30 @@ if __name__ == '__main__':
         lsystem = json.load(json_file)
 
     # The first iteration is always 'start'
-    steps = {0: lsystem['start']}
+    steps = [lsystem['start']]
 
     # Apply the rules at each iteration
-    for i in range(1, lsystem['iter']+1):
-        steps[i] = apply_rules(lsystem['rules'], steps[i-1])
+    for i in range(1, lsystem['iter']):
+        steps.append(apply_rules(lsystem['rules'], steps[i-1]))
 
     # Create drawing environment
-    wn = turtle.Screen()
-    pen = turtle.Turtle()
+    output_file("fig.html", title=system_file)
+    p = figure(title=system_file)
+    state = State(lsystem['angle'])
+
+    # Parse steps and convert to drawing instructions
+    for step in steps[-1]:
+        step_func = parse_step(step, state)
+        step_func(state)
+
+    xs = [step.x for step in state.steps]
+    ys = [step.y for step in state.steps]
+
+    # Draw lines
+    p.line(xs, ys, line_width=2)
+
+    show(p)
 
     # This is ugly right now and I should change it - maybe refactor
     # steps to a list
-    print(steps[lsystem['iter']])
-    draw_state(steps[lsystem['iter']], pen)
-
-    turtle.mainloop()
+    # print(steps[lsystem['iter']])
